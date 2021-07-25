53054be6440be8afa1cf748995730dc95d57cce5
ismailPervez <freelanceismail001@gmail.com>
2021-07-25 22:48:12 +0300

    this is the 8th commit - just added the animations of the what-we-do section using jquery

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-// jquery set up for animations
-console.log('js is working, over!');
+// select the icon image tags
+$('article img').click((event) => {
+    // console.log(event.target.id);
+    let icon = event.target.id;
+    $(`#${icon}`).fadeOut();
+    $(`.${icon}`).fadeIn(2000);
+    $(`.${icon}`).css("display", "block");
+})
 
+$('.what-we-do article p').click((event) => {
+    // console.log(event.target.className);
+    let desc = event.target.className;
+    $(`.${desc}`).fadeOut();
+    $(`.${desc}`).css("display", "none");
+    $(`#${desc}`).fadeIn();
+})
