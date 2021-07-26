452998c67cfc048875277175d2694d7e4a13cfc4
ismailPervez <freelanceismail001@gmail.com>
2021-07-26 08:34:38 +0300

    addded some functionality to the contact form, user is alerted when contact form is submitted and also there is form validation

--- script.js
+++ script.js
@@ -18,7 +18,23 @@ $('.what-we-do article p').click((event) => {
 /* hover animations on the portfolio projects card */
 $('.portfolio article').hover(function() {
 
-    $(this).find('.overlay').addClass('active')
+    $(this).find('.overlay').addClass('active');
 }, function() {
-    $(this).find('.overlay').removeClass('active')
+    $(this).find('.overlay').removeClass('active');
+})
+
+/* contact form verification and response */
+$('form').submit(function(event) {
+    event.preventDefault();
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+
+    if (!name || !email || !message) {
+        alert('all fields are required');
+    }
+
+    else {
+        alert(`hello ${name}! We have received your email. thank you for contacting us`);
+    }
 })
